424dd55bb9e960993611c4cefd8fb2143e5700d6
Thomas Yoho <TYohoJr@Gmail.com>
2018-02-17 14:24:18 -0700

    deleted unused files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Jumbotron, Button } from 'reactstrap';
-import Shelter from "./shelter.js";
-import Weather from "./weather.js";
 import axios from "axios";
 import Dropdown1 from "./dropdown.js";
 
