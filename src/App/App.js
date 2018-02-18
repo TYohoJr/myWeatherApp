7fbd942a7bf1fa19f2451a9cb3d4672ddecc4636
Thomas Yoho <TYohoJr@Gmail.com>
2018-02-17 20:08:25 -0700

    fixed infinite looping the API! :)

--- src/App/App.js
+++ src/App/App.js
@@ -19,6 +19,7 @@ export default class App extends Component {
               <Dropdown1/>
             </div>
         </Jumbotron>
+{/*Need to figure out how to make this stick to the bottom*/}        
         <footer>
           <p>Created by: Thomas Yoho</p>
           <p><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank">Click Here</a> for the Secret to Life</p>
--- src/dropdown/dropdown.js
+++ src/dropdown/dropdown.js
@@ -1,6 +1,11 @@
 // More cities will be added later
-// The rest are placeholders for now
+// Limit testing/saving whileserver is runnng to avoid API request lockouts
 // Use this URL to make sure the API is working - http://api.openweathermap.org/data/2.5/weather?q=bozeman&APPID=df763ac8b1b29ebbbf6e5d41aa8d44eb
+// My API key - df763ac8b1b29ebbbf6e5d41aa8d44eb
+// Random persons API key - bd5e378503939ddaee76f12ad7a97608
+// Rebecca's API key - deb29dfd065c544e4164e76b251706d3 (do not use unless necessary)
+// Currently accesses the API more than it needs to, but no longer accesses it in an infinite loop :)
+// Currently it accesses the API twice for every city selected, and twice each timeyou open the dropdown menu(besides the first time on page load)
 
 import React from 'react';
 import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
@@ -13,10 +18,14 @@ export default class Dropdown1 extends React.Component {
     super(props);
     this.toggle = this.toggle.bind(this);
     this.handleClick = this.handleClick.bind(this);
-    this.tableThing = ""
+    this.axiosGet = this.axiosGet.bind(this);    
+    this.counter = 0;
     this.state = {
+      tableThing:"\ ",
       dropdownOpen: false,
-      testVar:"placeholder",
+      testVar:2,
+// This is the entire data tree from OpenWeatherApp
+// Most data will never be used, but its accessible here in case of future needs      
       data:{
         coord:{
           lon:"Loading...",
@@ -74,14 +83,36 @@ export default class Dropdown1 extends React.Component {
     });
   }
 
-  componentDidMount(){
+  axiosGet(){
+    console.log("test2");
 // Need to change the format of the URL to accept either city Names or city ID's
-      axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${this.state.testVar}&APPID=df763ac8b1b29ebbbf6e5d41aa8d44eb`).then((response)=>{
+      axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${this.state.testVar}&APPID=bd5e378503939ddaee76f12ad7a97608`).then((response)=>{
         response.data.main.temp = Math.floor((response.data.main.temp - 273.15)* 1.8000 + 32.00)
+        debugger;
         this.setState({
-          data:response.data
-        })
+          data:response.data,
+          tableThing: <Table>
+              <thead>
+                <tr>
+                  <th>City</th>
+                  <th>Weather</th>
+                  <th>Temp (F)</th>
+                  <th>Humidity (%)</th>
+                  <th>Cloud Cover (%)</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>{this.state.data.name}</td>
+                  <td>{this.state.data.weather.main}</td>
+                  <td>{this.state.data.main.temp}</td>
+                  <td>{this.state.data.main.humidity}</td>
+                  <td>{this.state.data.clouds.all}</td>
+                </tr>
+              </tbody>
+            </Table>
       })
+    })
   }
   
   handleClick(somecityy) {
@@ -91,32 +122,15 @@ export default class Dropdown1 extends React.Component {
   }
 
   render() {
-      if(this.state.testVar !== "placeholder"){
-// Is there a better way to make this table?
-          this.tableThing = <Table>
-          <thead>
-            <tr>
-              <th>City</th>
-              <th>Weather</th>
-              <th>Temp (F)</th>
-              <th>Humidity (%)</th>
-              <th>Cloud Cover (%)</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>{this.state.data.name}</td>
-              <td>{this.state.data.weather.main}</td>
-              <td>{this.state.data.main.temp}</td>
-              <td>{this.state.data.main.humidity}</td>
-              <td>{this.state.data.clouds.all}</td>
-            </tr>
-          </tbody>
-        </Table>
-// Somehow need to get rid of this
-          this.componentDidMount()
-      }
-// Need to somehow do this using setState instead of redfining the current object
+// Surely there is a way to stop inifinite loops here without a counter...    
+    if ((typeof this.state.testVar !== "number") && this.counter < 2){
+      this.counter = this.counter + 1;
+      this.axiosGet()
+// This lets the user change what city they pick
+    } else {
+      this.counter = 0
+    }
+// Need to somehow do this using setState instead of redefining the current object
 //      for(var key in this.state.data){
 //        if(!this.state.data[key]){
 //          this.state.data[key] = "Unavailable"
@@ -134,22 +148,20 @@ export default class Dropdown1 extends React.Component {
           <DropdownToggle caret>
             Dropdown
           </DropdownToggle>
-        <div>{this.testVar}</div>
         <DropdownMenu>
-{/*Some way to automate making this dropdownlist?*/}
           <DropdownItem header>City</DropdownItem>
 {/*Need to change from citynames to city ID's*/}
           <DropdownItem onClick={() => this.handleClick("anaconda")}>Anaconda</DropdownItem>
           <DropdownItem onClick={() => this.handleClick("baker")}>Baker</DropdownItem>
           <DropdownItem onClick={() => this.handleClick("chicago")}>Chicago (test)</DropdownItem>
           <DropdownItem onClick={() => this.handleClick("billings")}>Billings</DropdownItem>
-          <DropdownItem onClcik={() => this.handleClick("boulder")}>Boulder</DropdownItem>
+          <DropdownItem onClick={() => this.handleClick("boulder")}>Boulder</DropdownItem>
           <DropdownItem onClick={() => this.handleClick("bozeman")}>Bozeman</DropdownItem>
           <DropdownItem onClick={() => this.handleClick("broadus")}>Broadus</DropdownItem>
           <DropdownItem onClick={() => this.handleClick("butte")}>Butte</DropdownItem>
         </DropdownMenu>
       </Dropdown>
-      <p>{this.tableThing}</p>
+      <div>{this.state.tableThing}</div>
     </div>
     );
   }
