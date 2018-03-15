f8de599fc34096ccecac3d8dbbba09efc1d1c4ed
Thomas Yoho <TYohoJr@Gmail.com>
2018-03-14 20:02:59 -0600

    css improvements

--- src/App/App.js
+++ src/App/App.js
@@ -5,22 +5,23 @@ import Dropdown1 from "../dropdown/dropdown.js";
 
 export default class App extends Component {
   render() {
-   return (
+    return (
       <div className="main-body">
         <div className="main-content">
-        <Jumbotron className="jumbotron-main">
-          <h1 className="main-header">Welcome to the Montana Weather App!</h1>
-          <h5 className="sub-header">Select your Montana based city to get a local weather report!</h5>
-            <hr/>
+          <Jumbotron className="jumbotron-main">
+            <h1 className="main-header">Welcome to the Montana Weather App!</h1>
+            <h5 className="sub-header">Select your Montana based city to get a local weather report!</h5>
+            <hr />
             <div className="dropdown">
-              <Dropdown1/>
+              <Dropdown1 />
             </div>
-        </Jumbotron>
-        </div>      
+          </Jumbotron>
+        </div>
         <footer>
-          <p>Created by: Thomas Yoho</p>
-          <p><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank" rel="noopener noreferrer">Click Here</a> for the Secret to Life</p>
-          <p>Weather data from OpenWeatherMap</p>
+          <div className="footer-contents">
+            <p>Created by: Thomas Yoho</p>
+            <p>Weather data from OpenWeatherMap</p>
+          </div>
         </footer>
       </div>
     )
